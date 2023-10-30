f308f8d433f3bdd7cff7b2a141773019c49d7b50
AlexTouchedGrass <dean-potts.622237@swl.k12.oh.us>
2023-10-30 09:44:36 -0400

    Nah losing it

--- src/NestedLoops.java
+++ src/NestedLoops.java
@@ -1,5 +1,27 @@
-public class Main {
+public class NestedLoops {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        for (int i = 1; i <= 5; i++ ) {
+
+            for (int j = 1; j <= i; j++ ) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        for (int x = 5; x >= 0; x--) {
+            for (int y = 1; y <= x; y++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        for (int x = 4; x >= 0; x--) {
+            for (int y = 1; y <= 5; y++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
     }
 }
